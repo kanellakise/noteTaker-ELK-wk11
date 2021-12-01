78df6ae42e8d4b3e337e3db20dbe214dc447f448
DeathByDusk <elijahkanellakis@gmail.com>
2021-12-01 14:18:48 -0600

    added server.js to start server, added apiRoutes index.js

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+// TODO: Add const for functions that create, get, and delete tasks
+
+const { notes } = require('../../db/db.json');
+
+
+//TODO: add 'router.get' to read notes in db
+
+//TODO: add 'router.post' to create a new note in db
+
+//TODO(OPTIONAL): add 'router.delete' to delete a note in db
+
+module.exports = router;
